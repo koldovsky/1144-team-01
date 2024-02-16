8d4bc16097adbe93f6e81cf907bd600d5b1d5b38
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2024-02-16 19:25:42 +0200

    Update tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -3,3 +3,15 @@
 function makeNegative(num) {
   return -Math.abs(num);
 }
+
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function makeNegative(num) {
+  return -Math.abs(num);
+}
+
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function makeNegative(num) {
+  return -Math.abs(num);
+}
