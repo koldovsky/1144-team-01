32a7577b367f6d27c7a2c867fac628a6012eb41c
Banduliak Andrii <109424920+AndriiBanduliak@users.noreply.github.com>
2024-03-01 22:35:32 +0000

    fix bugs partners (block_3-4)

--- js/partners.js
+++ js/partners.js
@@ -1,121 +1,43 @@
-const leftArrow = document.querySelector(".left-arrow");
-const rightArrow = document.querySelector(".right-arrow");
-const carousel = document.querySelector(".carousel");
-const images = document.querySelectorAll(".carousel-image");
-const totalImages = images.length;
-const imageWidth = 520;
-const transitionDuration = 0.8; 
+const slides = [
+  '<img class="carousel-image" src="img/partner1.png" alt="Image 1">',
+  '<img class="carousel-image" src="img/partner2.png" alt="Image 2" >',
+  '<img class="carousel-image" src="img/partner3.png" alt="Image 3">',
+  '<img class="carousel-image" src="img/partner4.png" alt="Image 4">'
+];
 
-const clonedImages = Array.from(images).map(image => image.cloneNode(true));
+let currentIdx = 0;
 
-clonedImages.forEach(clonedImage => {
-  carousel.appendChild(clonedImage);
-});
-
-let isDragging = false;
-let startX;
-let dragOffset = 0;
-let currentIndex = 0;
-
-carousel.addEventListener("mousedown", (event) => {
-  isDragging = true;
-  startX = event.clientX;
-  dragOffset = 0;
-});
-
-carousel.addEventListener("mousemove", (event) => {
-  if (isDragging) {
-    const endX = event.clientX;
-    dragOffset = startX - endX;
-    const transformValue = -currentIndex * imageWidth - dragOffset;
-    carousel.style.transition = "none";
-    carousel.style.transform = `translateX(${transformValue}px)`;
-  }
-});
-
-carousel.addEventListener("mouseup", () => {
-  if (isDragging) {
-    isDragging = false;
-    carousel.style.transition = "transform " + transitionDuration + "s cubic-bezier(0.25, 0.46, 0.45, 0.94)";
-    
-    if (Math.abs(dragOffset) > imageWidth / 2) {
-      currentIndex += dragOffset > 0 ? 1 : -1;
+function renderSlide() {
+    const slideContainer = document.querySelector('.carousel-container .carousel');
+    slideContainer.innerHTML = slides[currentIdx];
+    if (window.matchMedia('(min-width: 768px)').matches) {
+        const secondSlideIdx = currentIdx + 1 >= slides.length ? 0 : currentIdx + 1;
+        slideContainer.innerHTML += slides[secondSlideIdx];
+        if (window.matchMedia('(min-width: 980px)').matches) {
+            const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
+            slideContainer.innerHTML += slides[thirdSlideIdx];
+        }
     }
-
-    const transformValue = -currentIndex * imageWidth;
-    carousel.style.transform = `translateX(${transformValue}px)`;
-
-    setTimeout(() => {
-      carousel.style.transition = "";
-      checkBuffer();
-    }, transitionDuration * 1000);
-  }
-});
-
-function checkBuffer() {
-  if (currentIndex === totalImages) {
-    currentIndex = 0;
-    carousel.style.transform = "translateX(0)";
-  }
-  else if (currentIndex === -1) {
-    currentIndex = totalImages - 1;
-    carousel.style.transform = `translateX(${-totalImages * imageWidth}px)`;
-  }
 }
 
-leftArrow.addEventListener("click", () => {
-  currentIndex--;
-  updateCarousel();
-});
+renderSlide();
 
-rightArrow.addEventListener("click", () => {
-  currentIndex++;
-  updateCarousel();
-});
-
-function updateCarousel() {
-  const transformValue = -currentIndex * imageWidth;
-  carousel.style.transition = "transform " + transitionDuration + "s cubic-bezier(0.25, 0.46, 0.45, 0.94)";
-  carousel.style.transform = `translateX(${transformValue}px)`;
-
-  setTimeout(() => {
-    carousel.style.transition = "";
-    checkBuffer();
-  }, transitionDuration * 1000);
+function nextSlide() {
+    currentIdx = currentIdx + 1 >= slides.length ? 0 : currentIdx + 1;
+    renderSlide();
 }
 
-// Можна також додати обробники для сенсорних подій для тач-пристроїв
-carousel.addEventListener("touchstart", (event) => {
-  isDragging = true;
-  startX = event.touches[0].clientX;
-  dragOffset = 0;
-});
+function prevSlide() {
+    currentIdx = currentIdx - 1 <  0 ? slides.length - 1 : currentIdx - 1;
+    renderSlide();
+}
 
-carousel.addEventListener("touchmove", (event) => {
-  if (isDragging) {
-    const endX = event.touches[0].clientX;
-    dragOffset = startX - endX;
-    const transformValue = -currentIndex * imageWidth - dragOffset;
-    carousel.style.transition = "none";
-    carousel.style.transform = `translateX(${transformValue}px)`;
-  }
-});
+// setInterval(nextSlide, 3000);
 
-carousel.addEventListener("touchend", () => {
-  if (isDragging) {
-    isDragging = false;
-    carousel.style.transition = "transform " + transitionDuration + "s cubic-bezier(0.25, 0.46, 0.45, 0.94)";
-    
-    if (Math.abs(dragOffset) > imageWidth / 2) {
-      currentIndex += dragOffset > 0 ? 1 : -1;
-    }
+const btnNext = document.querySelector('.arrow-button.left-arrow');
+btnNext.addEventListener('click', nextSlide);
 
-    const transformValue = -currentIndex * imageWidth;
-    carousel.style.transform = `translateX(${transformValue}px)`;
+const btnPrev = document.querySelector('.arrow-button.right-arrow');
+btnPrev.addEventListener('click', prevSlide);
 
-    setTimeout(() => {
-      carousel.style.transition = "";
-      checkBuffer();
-    }, transitionDuration * 1000);
-  }
-});
+window.addEventListener('resize', renderSlide);
